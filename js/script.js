81535ea71c02fe23a956b8ac03236f2d8b1d02b9
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-03-24 08:22:27 +0700

    FEAT Add desktop design
    - CSS media query
    - window resize event listener

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const questionTexts = document.querySelectorAll(".question");
 const expendButtons = document.querySelectorAll(".expend-button");
 const buttonIcons = document.querySelectorAll(".button-icon");
 const answers = document.querySelectorAll(".answer");
+const pattern = document.getElementById("pattern");
 
 function toggleAnswer(element,button) {
     const controlledAnswerId = button.getAttribute("aria-controls");
@@ -23,6 +24,19 @@ function toggleAnswer(element,button) {
     }
 }
 
+function updatePattern () {
+    if (window.innerWidth > 750) {
+        pattern.src = "./assets/images/background-pattern-desktop.svg";
+    }
+    else {
+        pattern.src = "./assets/images/background-pattern-mobile.svg";
+    }
+}
+
+updatePattern();
+
+window.addEventListener("resize", updatePattern);
+
 expendButtons.forEach((button) => {
    button.addEventListener("click", ()=> {
     toggleAnswer(null,button);
@@ -34,3 +48,4 @@ questionTexts.forEach((question, index) => {
         toggleAnswer(question, expendButtons[index])
     })
 })
+
